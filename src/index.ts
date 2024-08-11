64c1ca1e677193d1e3f23c33dffa3e9a33bebb48
davit-khachatryan1 <khachatryandavit55@gmail.com>
2024-08-12 03:36:10 +0400

    d

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,15 @@
 import express from 'express';
+import cors from 'cors';
 import transactionRoutes from './routes/transaction';
-
 import { config } from './config';
 
 const app = express();
+
+app.use(cors());
+
 app.use(express.json());
 
-app.use('/api/transaction', transactionRoutes);
+app.use('/send-transaction', transactionRoutes);
 
 const PORT = 5000;
 app.listen(PORT, () => {
--- src/services/transactionService.ts
+++ src/services/transactionService.ts
@@ -1,17 +1,20 @@
-import { ethers } from 'ethers';
-import { config } from '../config';
+import { ethers } from "ethers";
+import { config } from "../config";
 
 const provider = new ethers.JsonRpcProvider(config.rpcUrl);
 const wallet = new ethers.Wallet(config.privateKey, provider);
 
-export const sendTransaction = async (recipient: string, customData: string): Promise<string> => {
-    const tx = {
-        to: recipient,
-        value: ethers.parseEther('0.01'), // Example: Sending 0.01 ETH
-        data: ethers.hexlify(ethers.toUtf8Bytes(customData)), // Custom data in transaction
-    };
+export const sendTransaction = async (
+  recipient: string,
+  customData: string
+): Promise<string> => {
+  const tx = {
+    to: recipient,
+    value: ethers.parseEther("0.01"),
+    data: ethers.hexlify(ethers.toUtf8Bytes(customData)),
+  };
 
-    const transactionResponse = await wallet.sendTransaction(tx);
-    await transactionResponse.wait(); // Wait for transaction confirmation
-    return transactionResponse.hash;
+  const transactionResponse = await wallet.sendTransaction(tx);
+  await transactionResponse.wait();
+  return transactionResponse.hash;
 };
