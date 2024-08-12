1d40f5600173384a59fc9719d8e84b1eb18c2b63
davit-khachatryan1 <khachatryandavit55@gmail.com>
2024-08-12 11:08:30 +0400

    2

--- dist/index.js
+++ dist/index.js
@@ -4,12 +4,12 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
-const dotenv_1 = __importDefault(require("dotenv"));
+const cors_1 = __importDefault(require("cors"));
 const transaction_1 = __importDefault(require("./routes/transaction"));
-dotenv_1.default.config();
 const app = (0, express_1.default)();
+app.use((0, cors_1.default)());
 app.use(express_1.default.json());
-app.use('/api/transaction', transaction_1.default);
+app.use('/send-transaction', transaction_1.default);
 const PORT = 5000;
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
--- dist/services/transactionService.js
+++ dist/services/transactionService.js
@@ -11,18 +11,17 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.sendTransaction = void 0;
 const ethers_1 = require("ethers");
-console.log(process.env.PRIVATE_KEY);
-const provider = new ethers_1.ethers.JsonRpcProvider(`https://polygon-amoy.infura.io/v3/e3b24eb7f51040338ccc84dfba3f201a`);
-console.log(provider.destroyed);
-const wallet = new ethers_1.ethers.Wallet('0777eca69099d9c5ddd5eca7be4cb7e002c872b010920913e29b334ebeaa9f73', provider);
+const config_1 = require("../config");
+const provider = new ethers_1.ethers.JsonRpcProvider(config_1.config.rpcUrl);
+const wallet = new ethers_1.ethers.Wallet(config_1.config.privateKey, provider);
 const sendTransaction = (recipient, customData) => __awaiter(void 0, void 0, void 0, function* () {
     const tx = {
         to: recipient,
-        value: ethers_1.ethers.parseEther('0.01'), // Example: Sending 0.01 ETH
-        data: ethers_1.ethers.hexlify(ethers_1.ethers.toUtf8Bytes(customData)), // Custom data in transaction
+        value: ethers_1.ethers.parseEther("0.01"),
+        data: ethers_1.ethers.hexlify(ethers_1.ethers.toUtf8Bytes(customData)),
     };
     const transactionResponse = yield wallet.sendTransaction(tx);
-    yield transactionResponse.wait(); // Wait for transaction confirmation
+    yield transactionResponse.wait();
     return transactionResponse.hash;
 });
 exports.sendTransaction = sendTransaction;
